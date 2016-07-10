f6f5d36442b48b5199f836a6a0f47c235222af74
Aaron Yool <aaronryool@gmail.com>
2016-07-10 15:32:59 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,7 @@
-const char main = 0xc3;
+#ifdef __linux__
+  const char main = 0xc3;
+#elif defined(_POSIX_VERSION)
+  const char main[] = "\xeb\xfe";
+#else
+#   error "it's your turn to submit a bug report lol"
+#endif
